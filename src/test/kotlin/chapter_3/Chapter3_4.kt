c8aff98d84bd6d5e52f24cf645dbbde1acb5da02
andannn <jqn296763005@gmail.com>
2025-12-24 13:28:45 +0900

    chapter 3.4

---
+++ src/test/kotlin/chapter_3/Chapter3_4.kt
@@ -0,0 +1,15 @@
+package chapter_3
+
+import AbstractPostgreSqlTest
+import withTransaction
+import kotlin.test.Test
+
+// https://www.postgresql.org/docs/current/tutorial-transactions.html
+class Chapter3_4 : AbstractPostgreSqlTest() {
+
+    @Test
+    fun transactions() = withTransaction {
+
+    }
+}
+
